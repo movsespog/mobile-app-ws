c06aa7ecfbfe336183016f1613510e2edf7852db
movses@novanoweb.com <movses@novanoweb.com>
2020-01-17 16:51:17 +0400

    Implementing Get User Details Web Service Endpoint

--- src/main/java/com/appsdeveloper/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/appsdeveloper/app/ws/service/impl/UserServiceImpl.java
@@ -63,6 +63,15 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    public UserDto getUserByUserId(String userId) {
+        UserDto userDto = new UserDto();
+        UserEntity userEntity = userRepository.findByUserId(userId);
+        if (userEntity == null) throw new UsernameNotFoundException(userId);
+        BeanUtils.copyProperties(userEntity, userDto);
+        return userDto;
+    }
+
+    @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         UserEntity userEntity = userRepository.findByEmail(email);
 
